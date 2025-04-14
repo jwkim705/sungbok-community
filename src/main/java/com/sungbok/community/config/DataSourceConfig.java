5243092b56508befe2db2b46105a8a1c5f892dab
jaewon <jwkim@rsupport.com>
2025-04-14 09:31:51 +0900

    1.redisinsight 추가 2.spring boot hikari cp 연결 추가 3.log4j2로 로깅 변경

---
+++ src/main/java/com/sungbok/community/config/DataSourceConfig.java
@@ -0,0 +1,27 @@
+package com.sungbok.community.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/application.yml")
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+      return new HikariConfig();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+      return new HikariDataSource(hikariConfig());
+    }
+}
+
+
